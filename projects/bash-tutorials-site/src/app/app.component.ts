6cc01cfdee96ccc0b432893498f8402c4be9d03f
TCARVI <tcarvi@me.com>
2023-08-18 00:34:55 -0300

    Angular site

---
+++ projects/bash-tutorials-site/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tcarvi-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bash-tutorials-site';
+}
