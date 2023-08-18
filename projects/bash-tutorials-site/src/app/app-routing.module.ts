3c26131cff69b1659b30ffcf5357a6e63db85dfb
TCARVI <tcarvi@me.com>
2023-08-18 01:53:43 -0300

    Acrescimo de componente

--- projects/bash-tutorials-site/src/app/app-routing.module.ts
+++ projects/bash-tutorials-site/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'front', loadChildren: () => import('./tcarvi-front/tcarvi-front.module').then(m => m.TcarviFrontModule) }];
+const routes: Routes = [{ path: 'front', loadChildren: () => import('./tcarvi/tcarvi-front/tcarvi-front.module').then(m => m.TcarviFrontModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
